994586fb7b4f9264bfbaef2e96c40454b7a86807
ThomasBrue <t.bruendl@gmail.com>
2021-09-27 23:11:24 +0200

    fully refactored

--- gildedrose/src/main/java/com/gildedrose/AdjustQuality.java
+++ gildedrose/src/main/java/com/gildedrose/AdjustQuality.java
@@ -1,14 +1,51 @@
 package com.gildedrose;
 
-public class AdjustQuality {
+public class AdjustQuality implements Products {
 
-    public int abc = 3;
+    public void updateItemQuality(Item item) {
+        if (!item.name.equals(BRIE) && !item.name.equals(BACKSTAGE)) {
+            notIncreaseSulfuras(item);
+        }
+        if (item.name.equals(BRIE) || item.name.equals(BACKSTAGE)) {
+            adjustItemQualityInRange(item, 1);
+            adjustBackstage(item);
+        }
+        if (!item.name.equals(SULFURAS)) {
+            item.sellIn = item.sellIn - 1;
+        }
+        if (item.sellIn < 0) {
+            itemOverdue(item);
+        }
+    }
 
-    private static final String BRIE = "Aged Brie";
-    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    private void notIncreaseSulfuras(Item item) {
+        if (!item.name.equals(SULFURAS)) {
+            adjustItemQualityInRange(item, -1);
+        }
+    }
 
-    public void adjustItemQualityIfInRange(Item item, int adjNum) {
+    private void adjustBackstage(Item item) {
+        if (item.sellIn < 11 && item.name.equals(BACKSTAGE)) {
+            adjustItemQualityInRange(item, 1);
+        }
+        if (item.sellIn < 6 && item.name.equals(BACKSTAGE)) {
+            adjustItemQualityInRange(item, 1);
+        }
+    }
+
+    private void itemOverdue(Item item) {
+        if (!item.name.equals(BRIE) && !item.name.equals(BACKSTAGE) && !item.name.equals(SULFURAS)) {
+            adjustItemQualityInRange(item, -1);
+        }
+        if (item.name.equals(BRIE)) {
+            adjustItemQualityInRange(item, 1);
+        }
+        if (item.name.equals(BACKSTAGE)) {
+            item.quality = item.quality - item.quality;
+        }
+    }
+
+    private void adjustItemQualityInRange(Item item, int adjNum) {
         int newItemQuality = item.quality + adjNum;
         if (newItemQuality <= 50 && newItemQuality >= 0) {
             item.quality = newItemQuality;
--- gildedrose/src/main/java/com/gildedrose/GildedRose.java
+++ gildedrose/src/main/java/com/gildedrose/GildedRose.java
@@ -2,74 +2,18 @@ package com.gildedrose;
 
 public class GildedRose {
 
-    private static final String BRIE = "Aged Brie";
-    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
     private Item[] items;
-
     AdjustQuality adjustQuality = new AdjustQuality();
 
     public GildedRose(Item... items) {
-        // tb.abc = 5;
         this.items = items;
     }
 
     public void updateQuality() {
         for (Item item : items) {
-            updateItemQuality(item);
+            adjustQuality.updateItemQuality(item);
         }
     }
-
-    private void updateItemQuality(Item item) {
-        if (!item.name.equals(BRIE) && !item.name.equals(BACKSTAGE)) {
-            notIncreaseSulfuras(item);
-        } else {
-            adjustItemQualityIfInRange(item, 1);
-            adjustBackstage(item);
-        }
-        if (!item.name.equals(SULFURAS)) {
-            item.sellIn = item.sellIn - 1;
-        }
-        if (item.sellIn < 0) {
-            itemOverdue(item);
-        }
-    }
-
-    private void notIncreaseSulfuras(Item item) {
-        if (!item.name.equals(SULFURAS)) {
-            adjustItemQualityIfInRange(item, -1);
-        }
-    }
-
-    private void adjustBackstage(Item item) {
-        if (item.sellIn < 11 && item.name.equals(BACKSTAGE)) {
-            adjustItemQualityIfInRange(item, 1);
-        }
-        if (item.sellIn < 6 && item.name.equals(BACKSTAGE)) {
-            adjustItemQualityIfInRange(item, 1);
-        }
-    }
-
-    private void itemOverdue(Item item) {
-        if (!item.name.equals(BRIE) && !item.name.equals(BACKSTAGE) && !item.name.equals(SULFURAS)) {
-            adjustItemQualityIfInRange(item, -1);
-        }
-        if (item.name.equals(BRIE)) {
-            adjustItemQualityIfInRange(item, 1);
-        }
-        if (item.name.equals(BACKSTAGE)) {
-            item.quality = item.quality - item.quality;
-        }
-    }
-
-    private void adjustItemQualityIfInRange(Item item, int adjNum) {
-        int newItemQuality = item.quality + adjNum;
-        if (newItemQuality <= 50 && newItemQuality >= 0) {
-            item.quality = newItemQuality;
-        }
-    }
-
-    // --------------------------------------------------------------------------------------------
 
     public Item[] getItems() {
         return items;
