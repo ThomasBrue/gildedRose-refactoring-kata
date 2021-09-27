47701cf722894308b1eb6da62f5e66dfdddd30db
ThomasBrue <t.bruendl@gmail.com>
2021-09-27 23:34:45 +0200

    not-refactored

--- gildedrose/src/main/java/com/gildedrose/Products.java
+++
@@ -1,7 +0,0 @@
-package com.gildedrose;
-
-public interface Products {
-    public static final String BRIE = "Aged Brie";
-    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
-}
